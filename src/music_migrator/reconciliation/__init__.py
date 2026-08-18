0aea5722543a459d918409dd374464bee817d422
Rani Nehme <42377545+raninehme@users.noreply.github.com>
2026-08-19 00:54:32 +0200

    Format reconciliation exports

--- src/music_migrator/reconciliation/__init__.py
+++ src/music_migrator/reconciliation/__init__.py
@@ -1,6 +1,10 @@
 """Compute desired collection state and reconciliation plans."""
 
-from music_migrator.reconciliation.planner import ReconciliationPlan, plan_playlist, plan_saved_tracks
+from music_migrator.reconciliation.planner import (
+    ReconciliationPlan,
+    plan_playlist,
+    plan_saved_tracks,
+)
 from music_migrator.reconciliation.policies import PlaylistMode
 
 __all__ = ["PlaylistMode", "ReconciliationPlan", "plan_playlist", "plan_saved_tracks"]
